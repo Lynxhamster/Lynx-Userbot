0cb097c31d3d5baf79b28910b9614285a09394e6
Kenzuuu <91766373+Kenzuuu@users.noreply.github.com>
2022-01-27 14:42:57 +0800

    Update help.py

--- userbot/modules/help.py
+++ userbot/modules/help.py
@@ -32,7 +32,7 @@ async def help(event):
         string = ""
         for i in CMD_HELP:
             string += "`" + str(i)
-            string += "`\t ✗  "
+            string += "`\t ✣  "
         await event.edit("**🔥ʙᴅʀʟ-ᴜsᴇʀʙᴏᴛ🔥**\n\n"
                          f"**◉ Bᴏᴛ ᴏꜰ {DEFAULTUSER}**\n**◉ Mᴏᴅᴜʟᴇꜱ : {len(modules)}**\n\n"
                          "**• Mᴀɪɴ Mᴇɴᴜ :**\n"
