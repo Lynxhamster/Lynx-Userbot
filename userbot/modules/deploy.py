3b53a2c0d27fd8e3799083ba9c09fe804ad834a1
Bdrl <93611653+Yansaii@users.noreply.github.com>
2022-01-29 01:31:18 +0700

    Update deploy.py

--- userbot/modules/deploy.py
+++ userbot/modules/deploy.py
@@ -46,7 +46,7 @@ async def _(event):
         "INFO - Bdrl-userbot: Logged in as 557667062",
         "INFO - Bdrl-userbot: Successfully...",
         "919852+00:00 app[worker.1]: 919 - Bdrl-userbot -",
-        "INFO -  ⚡️Bdrl-Userbot⚡️  ⚙️ V5.0 [TELAH DIAKTIFKAN!]",
+        "INFO -  ☠𝕭𝖉𝖗𝖑-𝖀𝖘𝖊𝖗𝖇𝖔𝖙☠  ⚙️ V5.0 [TELAH DIAKTIFKAN!]",
         "**Build Succeeded**"
 
     ]
