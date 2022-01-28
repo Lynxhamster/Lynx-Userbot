740c6ceb3990f821708e6c38f8cb5d67fdc8a3b4
Bdrl <93611653+Yansaii@users.noreply.github.com>
2022-01-29 01:06:33 +0700

    Up

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -27,7 +27,7 @@ for module_name in ALL_MODULES:
 
 
 LOGS.info(
-    f"⚡️Bdrl-Userbot⚡️ ⚙️ V{BOT_VER} [TELAH DIAKTIFKAN!]")
+    f"☠𝔅𝔡𝔯𝔩-𝔘𝔰𝔢𝔯𝔟𝔬𝔱☠ ⚙️ V{BOT_VER} [TELAH DIAKTIFKAN!]")
 
 
 if len(argv) not in (1, 3, 4):
