75d524faa52b1c350887785fa45d49dfc4d43fd4
Bdrl <93611653+Yansaii@users.noreply.github.com>
2022-02-03 19:23:18 +0700

    Update help.py

--- userbot/modules/help.py
+++ userbot/modules/help.py
@@ -36,6 +36,6 @@ async def help(event):
         await event.edit("**☠𝕭𝖉𝖗𝖑-𝖀𝖘𝖊𝖗𝖇𝖔𝖙☠**\n\n"
                          f"**◉ Bᴏᴛ ᴏꜰ {DEFAULTUSER}**\n**◉ Mᴏᴅᴜʟᴇꜱ : {len(modules)}**\n\n"
                          "**• Mᴀɪɴ Mᴇɴᴜ :**\n"
-                         f"◉ {string}◉\n\n✐ **ɴᴏᴛᴇꜱ :**  `.help animasi`\n☞  Onwer : @Bdrllll")
+                         f"◉ {string}◉\n\n✐ **ɴᴏᴛᴇꜱ :**  `.help animasi`\n☞  Onwer : @BukanBdrl")
         await asyncio.sleep(1000)
         await event.delete()
