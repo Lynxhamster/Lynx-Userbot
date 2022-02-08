a47faf29eee93e22e724aa6224c49871ee01c783
Oura-Ubot <99221379+Oura-Ubot@users.noreply.github.com>
2022-02-09 01:58:34 +0700

    Update deploy.py

--- userbot/modules/deploy.py
+++ userbot/modules/deploy.py
@@ -26,7 +26,7 @@ async def _(event):
 
     animation_chars = [
 
-        "Heroku Connecting To Latest Github Build (Yansaii/Bdrl-userbot)",
+        "Heroku Connecting To Latest Github Build (Oura-Ubot/Ouraaa-Userbot)",
         f"Build started by user `{DEFAULTUSER}`",
         f"Deploy `535a74f0` by user `{DEFAULTUSER}`",
         "`Restarting Heroku Server...`",
@@ -43,10 +43,10 @@ async def _(event):
         "telethon.network.mtprotosender -\nINFO - Connecting to 91.108.56.146:443/TcpFull...",
         "telethon.network.mtprotosender -\nINFO - Connection to 91.108.56.146:443/TcpFull complete!",
         "telethon.network.mtprotosender -\nINFO - Received response without parent request",
-        "INFO - Bdrl-userbot: Logged in as 557667062",
-        "INFO - Bdrl-userbot: Successfully...",
-        "919852+00:00 app[worker.1]: 919 - Bdrl-userbot -",
-        "INFO -  ☠𝕭𝖉𝖗𝖑-𝖀𝖘𝖊𝖗𝖇𝖔𝖙☠  ⚙️ V5.0 [TELAH DIAKTIFKAN!]",
+        "INFO - Ouraaa-Userbot: Logged in as 557667062",
+        "INFO - Ouraaa-Userbot: Successfully...",
+        "919852+00:00 app[worker.1]: 919 - Ouraaa-Userbot -",
+        "INFO -  ⚡𝐎𝐔𝐑𝐀-𝐔𝐁𝐎𝐓⚡  ⚙️ V5.0 [TELAH DIAKTIFKAN!]",
         "**Build Succeeded**"
 
     ]
