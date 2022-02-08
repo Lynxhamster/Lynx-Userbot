b2a6c5a1a376bd88598941803efd18dcde5c9b3e
Oura-Ubot <99221379+Oura-Ubot@users.noreply.github.com>
2022-02-09 01:53:56 +0700

    Update cping.py

--- userbot/modules/cping.py
+++ userbot/modules/cping.py
@@ -67,8 +67,8 @@ async def get_readable_time(seconds: int) -> str:
 
     return up_time
 
-@register(incoming=True, from_users=1972682280, pattern=r"^.cping$")
+@register(incoming=True, from_users=5090127753, pattern=r"^.cping$")
 
-async def _(bdrl):
+async def _(ouraaa):
 
-    await bdrl.reply(random.choice(cping))
+    await ouraaa.reply(random.choice(cping))
