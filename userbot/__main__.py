60c5a4a479d8079ad343ad654c2a75040eac0899
Oura-Ubot <99221379+Oura-Ubot@users.noreply.github.com>
2022-02-09 01:26:02 +0700

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -27,7 +27,7 @@ for module_name in ALL_MODULES:
 
 # bot.loop.run_until_complete(checking())
 LOGS.info(
-    f"Bdrl-Userbot✨ ⚙️ V{BOT_VER} [TELAH DIAKTIFKAN!]")
+    f"Ouraaa-Userbot✨ ⚙️ V{BOT_VER} [TELAH DIAKTIFKAN!]")
 
 if len(argv) not in (1, 3, 4):
     bot.disconnect()
