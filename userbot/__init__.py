f18520cdf7f3b8f0114ce1eba488b77bdc1ac422
Oura-Ubot <99221379+Oura-Ubot@users.noreply.github.com>
2022-02-09 03:16:15 +0700

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -125,7 +125,7 @@ UPSTREAM_REPO_URL = os.environ.get(
     "UPSTREAM_REPO_URL",
     "https://github.com/Oura-Ubot/Ouraaa-Userbot")
 UPSTREAM_REPO_BRANCH = os.environ.get(
-    "UPSTREAM_REPO_BRANCH", "Ouraaa-userbot")
+    "UPSTREAM_REPO_BRANCH", "Ouraaa-Userbot")
 
 # Console verbose logging
 CONSOLE_LOGGER_VERBOSE = sb(os.environ.get("CONSOLE_LOGGER_VERBOSE", "False"))
