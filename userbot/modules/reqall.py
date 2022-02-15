f26731f5ae7db9765a7aa0c72a72e6af5987d6c0
Oura-Ubot <99221379+Oura-Ubot@users.noreply.github.com>
2022-02-15 16:14:18 +0700

    Create reqall.py

---
+++ userbot/modules/reqall.py
@@ -0,0 +1,196 @@
+from platform import uname
+from userbot import ALIVE_NAME, CMD_HELP
+from userbot.events import register
+
+# ================= CONSTANT =================
+DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else uname().node
+# ============================================
+
+
+@register(outgoing=True, pattern='^.dz(?: |$)(.*)')
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    await typew.edit("**MINIMAL HARGAIN YANG GIKES TOLOL!!CUIH... NAJISSS**")
+
+
+@register(outgoing=True, pattern='^.ez(?: |$)(.*)')
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    await typew.edit("**ULANG KOK MANDANG VIEWS SIH ANJING!!!**")
+
+
+@register(outgoing=True, pattern='^.fz(?: |$)(.*)')
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    await typew.edit("**CUIHHHH, SI PALING BENER😃!!**")
+
+
+@register(outgoing=True, pattern='^.iz(?: |$)(.*)')
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    await typew.edit("**ULANG SOALNYA ANJING!!BURUANNN...**")
+
+
+@register(outgoing=True, pattern='^.rz(?: |$)(.*)')
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    await typew.edit("**DIA GA SEBAR LINK NGENTOTTT...!!**")
+
+
+@register(outgoing=True, pattern='^.tz(?: |$)(.*)')
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    await typew.edit("**SOALNYA MANA ANJING, GAUSA LAMA DEH SETANNN!!!**")
+
+
+@register(outgoing=True, pattern='^.uz(?: |$)(.*)')
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    await typew.edit("**TETAP GIKES WALAUPUN GAPERNAH DAPET GIPEWE, YAHAHAHA UDIN**")
+
+
+@register(outgoing=True, pattern='^.wz(?: |$)(.*)')
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    await typew.edit("**CIHH..GRUB SAMPAH!!! NURUNIN GIPEWE MAU SOALNYA ATO JAWABANNYA GA DANTA SEMUA!!**")
+
+
+@register(outgoing=True, pattern='^.zz(?: |$)(.*)')
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    await typew.edit("**CIE...HAYOLOO TREMOR GA SEBAR LINK YAA, AWOKWOKWOK!!**")
+
+
+@register(outgoing=True, pattern='^.kz(?: |$)(.*)')
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    await typew.edit("**GIKES GOBLOK!! JANGAN MAU GIPEWEE NYA DOANG TOLOL!!**")
+
+
+@register(outgoing=True, pattern='^.nz(?: |$)(.*)')
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    await typew.edit("**FREE IN GUA DONG ETMIN SAYANG🐣**")
+
+
+@register(outgoing=True, pattern='^.bz(?: |$)(.*)')
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    await typew.edit("**DIMANA ADA GIPEWEE DISITU ADA AING!!😎**")
+
+
+@register(outgoing=True, pattern='^.mz(?: |$)(.*)')
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    await typew.edit("**DUARRR MEMEKK!! MANA SOALNYA KONTOLLLL**")
+
+
+@register(outgoing=True, pattern='^.cz(?: |$)(.*)')
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    await typew.edit("**PAANSI PRIK BANGET NGENTOT!!MATI KEK LU BANGSAT!!**")
+
+
+@register(outgoing=True, pattern='^.sz(?: |$)(.*)')
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    await typew.edit("**CUIHHH!!! SOALNYA GA DANTA MONYET!!**")
+
+
+@register(outgoing=True, pattern='^.vz(?: |$)(.*)')
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    await typew.edit("**GIPEWENYA DRAMA TEROSS, KEK INDOSIAR AJA NGENTOT!!CUIHH...**")
+
+
+@register(outgoing=True, pattern='^.jz(?: |$)(.*)')
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    await typew.edit("**MISKIN MISKIN AJA NYET, GAUSA SOK SOAN JADI DONATUR!!**")
+
+
+@register(outgoing=True, pattern='^.az(?: |$)(.*)')
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    await typew.edit("**MINIMAL KALO HADIAHNYA DIKIT, SOALNYA DANTA YA NYET!!😁**")
+
+
+@register(outgoing=True, pattern='^.gz(?: |$)(.*)')
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    await typew.edit("**TURUNIN BANSOS NYA SEKARANG ANJING!! BELOM MAKAN SATU TAHUN NI....**")
+
+
+@register(outgoing=True, pattern='^.yz(?: |$)(.*)')
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    await typew.edit("**BELI KERIPIK DI WA ENTOT, CUIHHH... PRIKK BANGET SI NGENTOT!!**")
+
+
+@register(outgoing=True, pattern='^.op(?: |$)(.*)')
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    await typew.edit("**PEMERINTAH OP ABIEZZZZZZZ...🔥 ADA LAWAN KAH NYET? 😏**")
+
+
+@register(outgoing=True, pattern='^.oz(?: |$)(.*)')
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    await typew.edit("**KATANYA MAU VIEWERS NAIK, TAPI SOALNYA KEK KONTOLLL!!!**")
+    
+    
+@register(outgoing=True, pattern='^.lz(?: |$)(.*)')
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    await typew.edit("**ALLO GAISS... DENGAN SAYA DI SINI PEMBURU GIPEWE, YAHAHAA UDIN**")
+
+
+CMD_HELP.update({
+    "toxic":
+    ".dz\
+\nUsage: Bacotin gila Orang.\
+\n\n.ez\
+\nUsage: Buat Orang gila Sok Keras.\
+\n\n.fz\
+\nUsage: Ngatain Orang Wkwkkw ketawa.\
+\n\n.iz\
+\nUsage: Kontol Orang Ngatain katanya.\
+\n\n.rz\
+\nUsage: Pantun Anjing setan.\
+\n\n.tz\
+\nUsage: Nyebutin Binatang katanya.\
+\n\n.uz\
+\nUsage: Biar Dikata Ganteng katanya.\
+\n\n.wz\
+\nUsage: Biar Dikata Cantik katanya.\
+\n\n.zz\
+\nUsage: Tremor Kan Lu pada.\
+\n\n.kz\
+\nUsage: Memperkenalkan kamu Diri.\
+\n\n.nz\
+\nUsage: Menanyakan orang Kabar.\
+\n\n.bz\
+\nUsage: Sok Kepinteran banget.\
+\n\n.mz\
+\nUsage: Gc Nya seperti kuburan.\
+\n\n.cz\
+\nUsage: Dia tuh jelek banget.\
+\n\n.sz\
+\nUsage: Hahaha sokap sih.\
+\n\n.vz\
+\nUsage: Merendah banget.\
+\n\n.az\
+\nUsage: Neangan Sleep Call.\
+\n\n.jz\
+\nUsage: Hujat yang mantap.\
+\n\n.gz\
+\nUsage: Kecakepan.\
+\n\n.yz\
+\nUsage: teruntuk orang stres.\
+\n\n.op\
+\nUsage: Kepedean.\
+\n\n.lz\
+\nUsage: Anak Setan.\
+\n\n.oz\
+\nUsage: Ngatain org gila."
+})
