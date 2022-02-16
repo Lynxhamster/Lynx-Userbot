2f4f238743271f7f7b6f148f4e409a25ed32f825
Oura-Ubot <99221379+Oura-Ubot@users.noreply.github.com>
2022-02-17 06:48:14 +0700

    Create requestkntl.py

---
+++ userbot/modules/requestkntl.py
@@ -0,0 +1,45 @@
+from platform import uname
+from userbot import ALIVE_NAME, CMD_HELP
+from userbot.events import register
+
+# ================= CONSTANT =================
+DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else uname().node
+# ============================================
+
+
+@register(outgoing=True, pattern='^.din(?: |$)(.*)')
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    await typew.edit("**HAI... PERKENALKAN NAMAKU DINO, AKU DARI MANA MANA, UNCH DEH 🤪**")
+
+
+@register(outgoing=True, pattern='^.noo(?: |$)(.*)')
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    await typew.edit("**KATA DINOO KAMU TUH CACIMO BANGET SIH...**")
+
+
+@register(outgoing=True, pattern='^.dno(?: |$)(.*)')
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    await typew.edit("**TAU GA SIH?? KALO DINO TUH LUCU BANGET 🤪**")
+    
+    
+    @register(outgoing=True, pattern='^.odn(?: |$)(.*)')
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    await typew.edit("**DINOOO... AKU PADAMU...🥺**")
+
+
+    
+CMD_HELP.update({
+    "requestan":
+    ".din\
+\nUsage: Punya Dino.\
+\n\n.noo\
+\nUsage: Punya Dino.\
+\n\n.dno\
+\nUsage: Punya Dino.\
+\n\n.odn\
+\nUsage: Punya Dino."
+})
