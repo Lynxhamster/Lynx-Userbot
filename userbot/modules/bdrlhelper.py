a6918e3724986fd32a59ee307bab32e6bb823157
Oura-Ubot <99221379+Oura-Ubot@users.noreply.github.com>
2022-02-19 07:35:53 +0700

    Update bdrlhelper.py

--- userbot/modules/bdrlhelper.py
+++ userbot/modules/bdrlhelper.py
@@ -12,16 +12,16 @@ DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else uname().node
 async def usit(e):
     await e.edit(
         f"**Halo {DEFAULTUSER} Jika Anda Tidak Tau Perintah Untuk Memerintah Ku Ketik** `.help` Atau Bisa Minta Bantuan Ke:\n"
-        "\n[Telegram](t.me/Bdrllll)"
-        "\n[Repo](https://github.com/Yansaii/Bdrl-userbot)"
-        "\n[Instagram](instagram.com/kechili.8)")
+        "\n[Telegram](t.me/Ekojuuuuu)"
+        "\n[Repo](https://github.com/Oura-Ubot/Ouraaa-Userbot)"
+        "\n[Instagram](instagram.com/usamah.19)")
 
 
 @register(outgoing=True, pattern="^.vars$")
 async def var(m):
     await m.edit(
         f"**Disini Daftar Vars Dari {DEFAULTUSER}:**\n"
-        "\n[DAFTAR VARS](https://raw.githubusercontent.com/Yansaii/Bdrl-userbot/Bdrl-userbot/varshelper.txt)")
+        "\n[DAFTAR VARS](https://raw.githubusercontent.com/Oura-Ubot/Ouraaa-Userbot/Ouraaa-Userbot/varshelper.txt)")
 
 
 CMD_HELP.update({
