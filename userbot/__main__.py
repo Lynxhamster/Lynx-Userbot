96afacac780dad02886672af08b6f55175fd18ec
Lynxhamster <101751990+Lynxhamster@users.noreply.github.com>
2022-03-17 05:41:31 +0800

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -27,7 +27,7 @@ for module_name in ALL_MODULES:
 
 # bot.loop.run_until_complete(checking())
 LOGS.info(
-    f"Ouraaa-Userbot✨ ⚙️ V{BOT_VER} [TELAH DIAKTIFKAN!]")
+    f"Lynx-Userbot💫 ⚙️ V{BOT_VER} [DAH AKTIFK YA CROT]")
 
 if len(argv) not in (1, 3, 4):
     bot.disconnect()
