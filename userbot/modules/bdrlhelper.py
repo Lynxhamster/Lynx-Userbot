2626a99a2bd02285795d750ec6ca30bcd67b623a
Lynxhamster <101751990+Lynxhamster@users.noreply.github.com>
2022-03-17 10:02:26 +0800

    Update bdrlhelper.py

--- userbot/modules/bdrlhelper.py
+++ userbot/modules/bdrlhelper.py
@@ -12,16 +12,16 @@ DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else uname().node
 async def usit(e):
     await e.edit(
         f"**Halo {DEFAULTUSER} Jika Anda Tidak Tau Perintah Untuk Memerintah Ku Ketik** `.help` Atau Bisa Minta Bantuan Ke:\n"
-        "\n[Telegram](t.me/Ekojuuuuu)"
-        "\n[Repo](https://github.com/Oura-Ubot/Ouraaa-Userbot)"
-        "\n[Instagram](instagram.com/usamah.19)")
+        "\n[Telegram](t.me/Lynxhamsters)"
+        "\n[Repo](https://github.com/Lynxhamster/Lynx-Userbot)"
+        "\n[Instagram](instagram.com/rzamaulnaptra)")
 
 
 @register(outgoing=True, pattern="^.vars$")
 async def var(m):
     await m.edit(
         f"**Disini Daftar Vars Dari {DEFAULTUSER}:**\n"
-        "\n[DAFTAR VARS](https://raw.githubusercontent.com/Oura-Ubot/Ouraaa-Userbot/Ouraaa-Userbot/varshelper.txt)")
+        "\n[DAFTAR VARS](https://raw.githubusercontent.com/Lynxhamster/Lynx-Userbot/Lynx-Userbot/varshelper.txt)")
 
 
 CMD_HELP.update({
